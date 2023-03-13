7231ccc98c1e4d81485f6156fac0f68ae0ca917d
tuapeuuoku <traperrokukurwa@wp.pl>
2023-03-13 15:21:52 +0100

    12345

--- Assets/ZombieBehaviour.cs
+++ Assets/ZombieBehaviour.cs
@@ -1,14 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieBehaviour : MonoBehaviour
 {
+    [SerializeField] float health, maxHealth = 4f;
     GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -18,5 +21,14 @@ public class ZombieBehaviour : MonoBehaviour
         Vector3 playerDirection = transform.position - player.transform.position;
         
         transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
