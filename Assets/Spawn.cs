163c87b3c39c328367510d452a95e626a47e0e50
tuapeuuoku <traperrokukurwa@wp.pl>
2023-04-17 16:06:26 +0200

    prawie działa

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -19,7 +19,7 @@ public class Spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.FindGameObjectsWithTag("Enemy").Length < 3)
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length < 1)
         {
             Instantiate(ZombiePefab, GetRandomSpawnPosition(), Quaternion.identity);
         }
--- Assets/ZombieBehaviour.cs
+++ Assets/ZombieBehaviour.cs
@@ -6,9 +6,16 @@ using UnityEngine.AI;
 
 public class ZombieBehaviour : MonoBehaviour
 {
+    public float sightRange = 15f;
+    public float hearRange = 5f;
+   
     int hp = 5;
+
     GameObject player;
     NavMeshAgent agent;
+
+    private bool playerVisible = false;
+    private bool playerHearable = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +26,34 @@ public class ZombieBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (hp > 0)
+        Vector3 raySource = transform.position + Vector3.up * 1.8f;
+        Vector3 rayDirection = player.transform.position - transform.position;
+        Debug.DrawRay(raySource, rayDirection);
+        RaycastHit hit;
+        if(Physics.Raycast(raySource, rayDirection, out hit, sightRange))
+        {
+            if (hit.transform.CompareTag("Player"))
+                playerVisible = true;
+            else 
+                playerVisible = false;
+            Debug.Log(hit.transform.gameObject.name);
+        }
+
+
+        Collider[] heardObjects = Physics.OverlapSphere(transform.position, hearRange);
+
+        playerHearable = false;
+        foreach(Collider collider in heardObjects)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                playerHearable= true;
+            }
+        }
+
+        agent.isStopped = !playerVisible && !playerHearable;
+
+        if (hp > 0 && playerVisible)
         {
             //transform.LookAt(player.transform.position);
             //Vector3 playerDirection = transform.position - player.transform.position;
